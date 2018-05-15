0d4778c36a0fba89bcb98f08debfdc208cb3fef8
le fléo <leofleury@laposte.net>
2018-05-15 20:03:18 +0200

    commit : waveform visualisation v1

--- app/src/main/java/ceri/m1ilsen/applicationprojetm1/ui/DisplaySignalActivity.java
+++ app/src/main/java/ceri/m1ilsen/applicationprojetm1/ui/DisplaySignalActivity.java
@@ -1,21 +1,38 @@
 package ceri.m1ilsen.applicationprojetm1.ui;
 
 import android.app.Activity;
+import android.graphics.Canvas;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import java.io.IOException;
+
 import ceri.m1ilsen.applicationprojetm1.R;
+import ceri.m1ilsen.applicationprojetm1.fragment.WaveformFragment;
+import ceri.m1ilsen.applicationprojetm1.soundfile.CheapWAV;
+
 
 public class DisplaySignalActivity extends AppCompatActivity {
+
+    String fichier;
+
+    public static class ImplementWaveformFragment extends WaveformFragment {}
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_signal);
         Toast.makeText(getApplicationContext(),getIntent().getStringExtra("fileName"),Toast.LENGTH_LONG).show();
+        ImplementWaveformFragment wf = new ImplementWaveformFragment();
+        wf.setFileName(getIntent().getExtras().getString("fileName"));
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.waveform, wf)
+                .commit();
+
     }
 
     @Override
